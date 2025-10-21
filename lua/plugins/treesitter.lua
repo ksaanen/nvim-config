88406fd12cdc1938021e898285328530b2c2ef9b
Kevin Saanen <kevin@saanen.me>
2025-10-21 08:42:55 +0200

    Fix treesitter config
    Fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,16 @@
 local treesitter = require("nvim-treesitter")
 treesitter.setup({
-	configs = {
-		build = ":TSUpdate",
-		ensure_installed = {
-			"lua",
-			"typescript",
-			"angular",
-			"json",
-			"html",
-			"scss",
-			"rust",
-		},
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = { enable = true },
-		indent = { enable = true },
+	ensure_installed = {
+		"lua",
+		"typescript",
+		"angular",
+		"json",
+		"html",
+		"scss",
+		"rust",
 	},
+	-- Autoinstall languages that are not installed
+	auto_install = true,
+	highlight = { enable = true },
+	indent = { enable = true },
 })
