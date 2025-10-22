855c57aa49413b5a1d96cacb6c97237827f1b26d
Kevin Saanen <kevin@saanen.me>
2025-10-22 12:54:36 +0200

    Smartcase searching
    Smartcase searching

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -8,3 +8,11 @@ vim.opt.winborder = "rounded"
 -- vim.g.netrw_banner = 0 -- Disables help banner at the top of the window
 vim.g.netrw_winsize = 25
 vim.g.netrw_liststyle = 3
+
+-- Smart searching
+-- '/pattern' will search case insensitive.
+-- '/Pattern' will search case sensitive because op the capital P.
+-- '/pattern\C' will search case sensitive
+-- '/Pattern\c' will search case insensitive
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
