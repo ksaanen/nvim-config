1adf33dcc222c7bd449dfad995b611d96bd686f4
Kevin Saanen <kevin@saanen.me>
2025-10-22 23:31:01 +0200

    Enable preview
    Enable preview

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -6,6 +6,7 @@ vim.opt.winborder = "rounded"
 
 -- Netrw settings
 -- vim.g.netrw_banner = 0 -- Disables help banner at the top of the window
+vim.g.netrw_preview = 1
 vim.g.netrw_winsize = 25
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 4
