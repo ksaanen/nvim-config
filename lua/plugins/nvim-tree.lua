c0e47525c478b76a5fb3d257b6535d307c781a39
Kevin Saanen <kevin@saanen.me>
2026-04-03 13:42:00 +0200

    add config for nvim-tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,15 @@
+local config = {
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+}
+require("nvim-tree").setup(config)
