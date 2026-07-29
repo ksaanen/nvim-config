1dd046d5495a2ded78140763c847fb7e73fe4c4e
Kevin Saanen <kevin@saanen.me>
2026-07-29 14:31:34 +0200

    Update nvim-tree.lua
    Focus on active buffer

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -13,3 +13,15 @@ local config = {
     },
 }
 require("nvim-tree").setup(config)
+
+-- Have nvim-tree focus on active buffer.
+local api = require("nvim-tree.api")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if (vim.fn.bufname() == "NvimTree_1") then return end
+
+        api.tree.find_file({ buf = vim.fn.bufnr() })
+    end,
+})
