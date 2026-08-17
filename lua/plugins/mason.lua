f0fda2c7a1b1ce89179bddbcbef29eedf12e1ebd
Kevin Saanen <kevin@saanen.me>
2026-08-17 10:51:16 +0200

    Add Mason config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+--[[
+:Mason - opens a graphical status window
+:MasonUpdate - updates all managed registries
+:MasonInstall <package> ... - installs/re-installs the provided packages
+:MasonUninstall <package> ... - uninstalls the provided packages
+:MasonUninstallAll - uninstalls all packages
+:MasonLog - opens the mason.nvim log file in a new tab window
+]]
+
+require('mason').setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
+	}
+})
