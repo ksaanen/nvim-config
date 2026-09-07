6bedae62a4a767f0ef76232934610ccb106967ac
Kevin Saanen <kevin@saanen.me>
2026-09-07 09:29:31 +0200

    Update nvim-tree.lua

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -17,12 +17,14 @@ require("nvim-tree").setup(config)
 local api = require("nvim-tree.api")
 
 -- Have nvim tree open on startup
+--[[
 api.tree.toggle({
     path = "<args>",
     find_file = false,
     update_root = false,
     focus = true,
 })
+]]
 
 -- Have nvim-tree focus on active buffer.
 vim.api.nvim_create_autocmd("BufEnter", {
